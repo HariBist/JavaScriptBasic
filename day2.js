14853e0d18947d441dfe34e278db31e6e40a3916
HariBist <haribist1001@gmail.com>
2024-03-04 11:07:46 +0545

    first commit

---
+++ day2.js
@@ -0,0 +1,68 @@
+
+// const numbers = [1,2,3,4,5]
+// slice-->> used to create a copy of a portion of the Array. it dosen't modify original
+// Array.
+// slice(startindex,endindex) where startindex inclusive and endindex is exclusive.
+// const newarray = numbers.slice(1,4);
+// console.log(numbers)
+// console.log(newarray);
+
+// ----splice--- it modify the original array
+// let spliceNumbers = numbers.splice(2,1,34,5)
+// console.log(spliceNumbers)
+// console.log(numbers)
+
+//split: it helps a sting to convert in array according its 
+// let text = "l love javascript"
+// const arraynew = text.split(" ")
+// console.log(arraynew)
+// console.log(arraynew.length)
+
+
+// const person = {
+//     name: "Hari Bista",
+//     addresss: "Mahendranagar ,Kanchanpur",
+//     age :32
+// }
+// Object.freeze(person)//it prevent the modify the object from outside
+// person.country = "nepal"
+// person.age=22
+// console.log(person)
+// // console.log(Object.keys(person))
+// console.log(Object.values(person))
+
+
+//Conditional in Javascript--
+
+// const isRanning = true
+// if(isRanning)
+// {
+//     console.log('carry umbrella')
+// }
+// else
+// {
+//     console.log("not need to carry umbrella")
+// }
+// const temp = 30
+// if(temp>30)
+// {
+//     console.log("it is hot outsider")
+// }
+// else if(temp>25 && temp<=30) {
+//     console.log("it is plesent outside")
+// }
+// else {
+//     console.log("it is cold")
+// }
+ 
+
+//ternary operator
+// const isRaining = true;
+// const result = isRaining ? "Carry umbrella":"Dont carry Umbrella"
+
+//short circuit evaluation
+// const result = isRaining && "Carry umbrella" || "Don't carry"
+// console.log(result)
+let isloggedIn = true
+const name = isloggedIn && "Authenticated"
+console.log(name)
---
+++ first.js
@@ -0,0 +1,19 @@
+// let khayo ={
+//     institueNmae :"hari pathsala",
+//     reactCourse : "dbms"
+// }
+// console.log(khayo)
+// const names= ['Hari','bist']
+//names[1] ="anish"
+// console.log(names[2])
+// console.log(names)
+// names.push("dipika")//this push method used to add item on last array
+// console.log(names)
+// names.pop()//this pop method remove the last item on the array form lastk
+// console.log(names)
+// names.unshift('Raju')//this method insert item start of the array
+// names.shift()//remove first element of the array
+// console.log(names)
+// console.log(names.length)
+// console.log(names[0].length)
+
