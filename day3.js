cafd3e47bb5decda9ce255123810142de2055372
HariBist <haribist1001@gmail.com>
2024-03-12 08:54:41 +0545

    hello

---
+++ day3.js
@@ -0,0 +1,107 @@
+//daay4
+
+// console.log(1+1)
+//function is decleared with keyword function 
+
+// function add(a,b,c)//these are parameter which accept garney kura j huxa these are parameter
+// {
+//     console.log(a+b+c)
+// }
+// add(1,5,6)//these are argument what are 
+
+
+//named function
+// let add = function(a,b,c){
+//     console.log(a+b+c)
+// }
+// add(1,2,3)
+
+//named arrow function
+
+// let add = (a,b,c=12) => console.log(a+b+c)//if you have multiple line you have to use curly brass
+
+// add(1,3)
+
+// function (a,b,c){
+//     console.log(hello world)
+// }
+// const callBack = (fn)=>{
+//     return isFinite
+// }
+
+// const anonymous = ()=>{
+//     console.log("hello")
+// }
+// anonymous()
+// function squre(a){
+//     // console.log(a*a)
+//     return a *a
+
+// }
+// const num =4
+// const a = squre(num)
+// console.log(a)
+
+// const squre=(a) => a *a
+// const a=5 
+// const result = squre(a)
+// console.log(result)
+// let c = function(b)
+// {
+//     return b *b;
+// }
+// let b=4;
+
+// let r = c(b)
+// console.log(r)
+
+//loop
+//doing any task repeatedy untill the condition met
+
+
+
+//for loop
+//  const friends = ['manish','bob',"rahul"]
+// for(var i =0;i<friends.length;i++)
+// {
+// console.log(friends[i])
+// }
+
+
+//while loop
+// let i=0
+// while(i<friends.length)
+// {
+//     console.log(friends[i])
+//     i++
+// }
+
+// for(haha of friends){
+//     console.log(haha)
+// }
+
+// for(f in friends)
+// {
+//     console.log(f, friends(f))
+// }
+// console.log(name)
+// // var name = "hari"
+// var name = "hari"
+// add()
+// function add()
+// {
+//     console.log("hello")
+// }
+// add()
+// // let add  = ()=>{
+// //     console.log(1+1)
+// // }
+// var add = function(){
+//     console.log(1+1)
+// }
+function example()
+{
+    console.log(y)
+   var y = 99 //output undefined
+}
+example()
