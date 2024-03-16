6813e27034ebd735b126eb896be5681060b84dab
HariBist <haribist1001@gmail.com>
2024-03-16 20:50:17 +0545

    init the method

---
+++ exampleOfMehod.js
@@ -0,0 +1,5 @@
+//here we e discuss about the methods that are used in an array in js
+
+let arr = [1,2,4,5,6];
+//join() method is here used to make the array a single output
+console.log(arr.join());
