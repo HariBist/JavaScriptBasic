b172b75ac2c8f02d07584e3dfa8764f77d9ead7c
HariBist <haribist1001@gmail.com>
2024-03-17 08:04:13 +0545

    add array method

--- exampleOfMehod.js
+++ exampleOfMehod.js
@@ -3,3 +3,16 @@
 let arr = [1,2,4,5,6];
 //join() method is here used to make the array a single output
 console.log(arr.join());
+// here are there way to add an new Element first position of array
+
+// arr.unshift("z");
+
+// arr.splice(0,0,"z");
+
+// arr =["z",...arr]
+
+
+//to add and element on array at end we simply use push
+arr.push('z')
+arr.pop();//remove the element from array
+console.log(arr)
